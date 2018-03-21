a986419d0acfa3eee758a5879454bd7b245c2a4d
ameerthehacker <ameerjhanprof@gmail.com>
2018-03-21 14:56:35 +0530

    Find the minimum spanning tree of a graph using Dijaktra algorithm :fire:

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,28 @@ let a = new Vertex("a");
 let b = new Vertex("b");
 let c = new Vertex("c");
 let d = new Vertex("d");
+let e = new Vertex("e");
+let f = new Vertex("f");
+let g = new Vertex("g");
+let h = new Vertex("h");
+let i = new Vertex("i");
 
-a.connect(b);
-a.connect(c);
-b.connect(c);
-c.connect(a);
-c.connect(d);
-d.connect(d);
+a.connect(b, 4);
+a.connect(h, 8);
+b.connect(h, 11);
+b.connect(c, 8);
+h.connect(g, 1);
+h.connect(i, 7);
+c.connect(i, 2);
+g.connect(i, 6);
+c.connect(d, 7);
+g.connect(f, 2);
+c.connect(f, 4);
+d.connect(f, 14);
+d.connect(e, 9);
+f.connect(e, 10);
 
-let g = new Graph();
-g.addVertices([a, b, c, d]);
+let graph = new Graph();
+graph.addVertices([a, b, c, d, e, f, g, h, i]);
 
-console.log(g.bfs("c"));
+console.log(graph.getSPT("a"));
--- src/js/graph.js
+++ src/js/graph.js
@@ -46,6 +46,52 @@ export class Graph {
   findVertexByName(name) {
     return this.vertices.find(vertex => vertex.name == name);
   }
+  getSPT(source) {
+    const sourceVertex = this.findVertexByName(source);
+    if (sourceVertex != null) {
+      let spt = {};
+      let set = {};
+      this.vertices.forEach(vertex => {
+        // If it is the source keep its distance as 0
+        if (vertex.name == source) {
+          set[vertex.name] = { distance: 0, parent: vertex.name };
+        } else {
+          set[vertex.name] = { distance: Infinity, parent: vertex.name };
+        }
+      });
+      // Keep doing stuffs untill all vertices are transfered to spt
+      while (Object.keys(set).length != Object.keys(spt).length) {
+        // Get the set difference between spt and set
+        let diffSet = {};
+        Object.keys(set).forEach(vertexName => {
+          if (spt[vertexName] === undefined) {
+            diffSet[vertexName] = set[vertexName];
+          }
+        });
+        // Find the vertex having minimum distance in set
+        let min = { name: "", distance: Infinity, parent: "" };
+        Object.keys(diffSet).forEach(vertexName => {
+          if (diffSet[vertexName].distance < min.distance) {
+            min.name = vertexName;
+            min.distance = diffSet[vertexName].distance;
+            min.parent = diffSet[vertexName].parent;
+          }
+        });
+        // Add the min vertex details to spt
+        spt[min.name] = { distance: min.distance, parent: min.parent };
+        let minVertex = this.findVertexByName(min.name);
+        // Update the adjacent vertices distance in the set
+        minVertex.edges.forEach(edge => {
+          let newDistance = min.distance + edge.distance;
+          if (newDistance < set[edge.toVertex.name].distance) {
+            set[edge.toVertex.name].distance = newDistance;
+            set[edge.toVertex.name].parent = minVertex.name;
+          }
+        });
+      }
+      return spt;
+    }
+  }
   bfs(source) {
     const sourceVertex = this.findVertexByName(source);
     const queue = new Queue();
